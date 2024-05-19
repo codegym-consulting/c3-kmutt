c845f0ff74391a2767df1eab09d54eae1bdc6775
Pitikorn Chulitawong <93501761+BeamPitikorn@users.noreply.github.com>
2024-05-19 21:32:28 +0700

    feat: bottom sheet for navbar

---
+++ components/common/BottomSheet.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "update:modelValue", value: boolean): void;
+}>();
+</script>
+
+<template>
+  <USlideover
+    :ui="{
+      base: 'relative flex-1 flex flex-col w-full focus:outline-none rounded-t-[40px] overflow-y-auto',
+      wrapper: 'inset-[unset] top-[50%] bottom-0',
+      width: 'max-w-full',
+      overlay: {
+        background: 'bg-[#3b3b3b]/80',
+      },
+      translate: {
+        base: 'translate-y-0 translate-x-0',
+        right:
+          'translate-y-full rtl:-translate-y-full translate-x-0 rtl:-translate-x-0',
+      },
+    }"
+    :model-value="props.modelValue"
+    @update:model-value="(val) => emit('update:modelValue', val)"
+  >
+    <slot></slot>
+  </USlideover>
+</template>

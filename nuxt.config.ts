82e8acfb99167ca8372de576f7828ec85d613495
Pitikorn Chulitawong <93501761+BeamPitikorn@users.noreply.github.com>
2024-05-23 00:50:35 +0700

    feat: add favicon / meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,29 @@ export default defineNuxtConfig({
     },
     "~/components",
   ],
+  app: {
+    head: {
+      title: "KMUTT Network knowledge sharing platform",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon/favicon.svg",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon/favicon.png",
+        },
+      ],
+    },
+  },
 });

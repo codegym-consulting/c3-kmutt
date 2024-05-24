d53b170645881776edec64f0282b1a54317f7f01
Pitikorn Chulitawong <93501761+BeamPitikorn@users.noreply.github.com>
2024-05-24 12:44:18 +0700

    feat: add IBM font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,6 +51,18 @@ export default defineNuxtConfig({
           type: "image/png",
           href: "/favicon/favicon.png",
         },
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&display=swap",
+        },
       ],
     },
   },

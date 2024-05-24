66fa3efd594ca32195a1908f02046942a76d0811
Juk <chukkrit.juk@gmail.com>
2024-05-25 01:22:20 +0700

    fix: default formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ export default withNuxt(
   {
     rules: {
       'vue/max-attributes-per-line': 'off',
+      'vue/html-indent': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
     },
   },
   // Your custom configs here

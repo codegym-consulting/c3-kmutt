31d85b993f22eb5232337e37356cb0a55501951a
Juk <chukkrit.juk@gmail.com>
2024-06-10 19:23:41 +0700

    feat: create mock resume page
    feat: create resume mock page

---
+++ pages/resume/create.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+definePageMeta({
+    layout: 'register',
+})
+
+const step = ref(1)
+const validate = reactive({
+    stepOne: false,
+    stepTwo: false,
+})
+
+const onClickNext = () => {
+
+}
+</script>
+
+<template>
+    <div class="flex flex-col h-full">
+        <h2 class="text-2xl leading-7 md:text-[32px] md:leading-10 font-bold text-gray-10">
+            Input Resume
+        </h2>
+        <p class="text-base leading-5 text-gray-7 mt-2 mb-4">
+            Upload your resume file for submission.
+        </p>
+        <div>option here</div>
+        <RegisterActionFooter :buttons="step === 1 ? ['', 'next'] : ['back', 'done']"
+            :disabled-next="step === 1 ? !validate.stepOne : !validate.stepTwo" :disabled-back="false"
+            @click-back="step--" @click-next="onClickNext" />
+    </div>
+</template>
---
+++ pages/resume/success.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+definePageMeta({
+    layout: 'blank'
+})
+</script>
+
+<template>
+    <div>
+        <div :class="$classes(
+            'absolute inset-0 m-auto w-full h-full -z-10',
+            'bg-[url(~/public/login/bg_mobile.webp)] md:bg-[url(~/public/login/bg_tablet.webp)] lg:bg-[url(~/public/login/bg.webp)]',
+            'bg-no-repeat bg-cover bg-center',
+        )" />
+        <UContainer class="flex justify-center absolute inset-0 h-fit m-auto">
+            <UCard data-aos="fade-up" as="article" :ui="{
+                strategy: 'override',
+                base: 'max-w-[820px] w-full',
+                divide: 'divide-y-0',
+                rounded: 'rounded-2xl',
+                body: {
+                    padding: 'px-4 py-16 md:pt-14 md:pb-[104px]',
+                },
+            }">
+                <div class="max-w-[820px] mx-auto text-center w-full flex flex-col items-center gap-y-14">
+                    <figure class="hidden md:block">
+                        <img src="/logo/logo_full.svg" alt="3c_logo">
+                    </figure>
+                    <div class="flex flex-col items-center gap-y-6">
+                        <UIcon name="prime:thumbs-up" class="w-12 h-12 text-gray-10" />
+                        <h1 class="heading-label">Resume successfully created!</h1>
+                        <p class="text-gray-10 text-base leading-5">
+                            Your resume template has been successfully created. Start showcasing your professional
+                            journey!
+                        </p>
+                    </div>
+                    <NuxtLink :to="'/workspace'" class="round-icon-btn w-[72px] h-[72px] [&_svg]:w-8 [&_svg]:h-8">
+                        <UIcon name="material-symbols:arrow-forward" />
+                    </NuxtLink>
+                </div>
+            </UCard>
+        </UContainer>
+    </div>
+</template>

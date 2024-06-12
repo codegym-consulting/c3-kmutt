f43c2fc943c2444f43967665e6b286eb2a787d58
Juk <chukkrit.juk@gmail.com>
2024-06-12 21:50:34 +0700

    feat: landing projects

---
+++ server/api/landing/projects.get.ts
@@ -0,0 +1,49 @@
+export default defineEventHandler(async () => {
+  try {
+    return [{
+        id: 1,
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1713823799895-bddbbf670531?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Developing machine learning models to predict spatial positions of objects across the human visualization with..',
+        category: 'Technology',
+        location: 'Bangkok'
+    },{
+        id: 2,
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1713823799895-bddbbf670531?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Developing machine learning models to predict spatial positions of objects across the human visualization with..',
+        category: 'Technology',
+        location: 'Bangkok'
+    },
+    {
+        id: 3,
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1713823799895-bddbbf670531?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Developing machine learning models to predict spatial positions of objects across the human visualization with..',
+        category: 'Technology',
+        location: 'Bangkok'
+    },
+    {
+        id: 4,
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1713823799895-bddbbf670531?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Developing machine learning models to predict spatial positions of objects across the human visualization with..',
+        category: 'Technology',
+        location: 'Bangkok'
+    },
+    {
+        id: 5,
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1713823799895-bddbbf670531?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Developing machine learning models to predict spatial positions of objects across the human visualization with..',
+        category: 'Technology',
+        location: 'Bangkok'
+    },
+    {
+        id: 6,
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1713823799895-bddbbf670531?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Developing machine learning models to predict spatial positions of objects across the human visualization with..',
+        category: 'Technology',
+        location: 'Bangkok'
+    }]
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+

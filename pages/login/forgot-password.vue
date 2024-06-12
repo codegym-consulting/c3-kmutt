59e29bf4b3966c2f430a5ef5e3c6568c0efc7797
Juk <chukkrit.juk@gmail.com>
2024-06-13 00:55:59 +0700

    feat: forgot password page

--- pages/login/forgot-password.vue
+++ pages/login/forgot-password.vue
@@ -9,16 +9,11 @@ definePageMeta({
 const register = useRegisterStore()
 
 const email = ref('')
-const password = ref('')
 const formValid = ref(false)
 
 const { error: emailError, validate: validateEmail } = useFieldValidation(
   loginSchema.shape.email,
   email,
-)
-const { error: passwordError, validate: validatePassword } = useFieldValidation(
-  loginSchema.shape.password,
-  password,
 )
 
 const onFocus = () => {
@@ -32,24 +27,14 @@ watchDebounced(
   },
   { debounce: 500 },
 )
-watchDebounced(
-  password,
-  () => {
-    validatePassword()
-  },
-  { debounce: 500 },
-)
 watch(
-  () => [email, password, emailError, passwordError],
-  ([_email, _password, _emailError, _passwordError]) => {
-    formValid.value =
-      !_emailError.value &&
-      !_passwordError.value &&
-      !!_email.value &&
-      !!_password.value
+  () => [email, emailError],
+  ([_email, _emailError]) => {
+    formValid.value = !_emailError.value && !!_email.value 
   },
   { deep: true },
 )
+
 </script>
 
 <template>
@@ -57,15 +42,10 @@ watch(
     <h2
       class="flex justify-between gap-x-2 text-2xl leading-7 md:text-[32px] md:leading-10 font-bold text-gray-10"
     >
-      Continue with email
-      <NuxtLink
-        class="text-xs md:text-base text-chrysler-blue underline font-normal"
-        to="/forgot-password"
-        >Forgot Password</NuxtLink
-      >
+      Forgot Password
     </h2>
     <p class="text-base leading-5 text-gray-7 mt-2 mb-4">
-      Sign in with your registered email address and password.
+        Please enter your email address to receive a password reset link.
     </p>
     <div class="flex flex-col gap-y-6 mt-6 md:mt-8">
       <Input
@@ -76,15 +56,6 @@ watch(
         :error="emailError"
         @focus="onFocus"
         @touched="validateEmail"
-      />
-      <Input
-        v-model="password"
-        label="Enter your password"
-        name="password"
-        type="password"
-        :error="passwordError"
-        @focus="onFocus"
-        @touched="validatePassword"
       />
     </div>
     <RegisterActionFooter

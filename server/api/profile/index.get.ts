a7d4610aa8ea1eeafebcd7e0ce3c390a0c8345ad
Juk <chukkrit.juk@gmail.com>
2024-06-30 02:08:00 +0700

    feat: join profile data

--- server/api/profile/index.get.ts
+++ server/api/profile/index.get.ts
@@ -3,8 +3,8 @@ import { getProfile } from "~/server/services/profile/get"
 export default defineEventHandler(async (event) => {
     const session = await requireUserSession(event)
     try {
-      const profiles = await getProfile(session.user.id)
-      return { yourProjects: 0, yourNetwork: 0, ...profiles[0] }
+      const profile = await getProfile(session.user.id)
+      return { yourProjects: 0, yourNetwork: 0, ...profile }
     } catch (error) {
       console.error(error)
       return null
--- server/services/profile/get.ts
+++ server/services/profile/get.ts
@@ -1,13 +1,24 @@
-import { profile } from "~/server/drizzle/schema"
+import { expertise, interest, profile } from "~/server/drizzle/schema"
 import db from "~/server/libs/pg"
-import { eq } from 'drizzle-orm';
+import { eq, inArray } from 'drizzle-orm';
 
 export const getProfile = async (userId: number) => {
     try {
-        return await db.select()
+        const [result] = await db.select()
                 .from(profile)
                 .limit(1)
                 .where(eq(profile.userId, userId))
+
+        const [areaOfInterests, expertises] = await Promise.all([
+            db.select({ label: interest.name, value: interest.id })
+                .from(interest)
+                .where(inArray(interest.id, result.areaOfInterests || [])),
+            db.select({ label: expertise.name, value: expertise.id })
+                .from(expertise)
+                .where(inArray(expertise.id, result.expertises || []))
+        ])
+
+        return { ...result, areaOfInterests, expertises }
     } catch (error) {
         console.error(`Error selecting profile: ${error}`)
         throw error

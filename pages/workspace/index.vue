724d57d2011729e949d3b55478920d386a9cccf8
Pitikorn Chulitawong <93501761+BeamPitikorn@users.noreply.github.com>
2024-06-30 15:55:36 +0700

    feat: client only to prevent any error from SSR request

--- pages/workspace/index.vue
+++ pages/workspace/index.vue
@@ -7,14 +7,26 @@ definePageMeta({
 const { $fetchApi } = useNuxtApp()
 const dashboardRepo = dashboardRepository($fetchApi)
 
-const { data: projects } = useAsyncData(
+const { data: projects, refresh: refreshProjects } = useAsyncData(
   'highlight-projects',
   dashboardRepo.getRecentProjects,
+  {
+    immediate: false,
+  },
 )
-
-const { data: profile } = useAsyncData(
+const { data: profile, refresh: refreshProfile } = useAsyncData(
   'dashboard-profile',
   dashboardRepo.getProfile,
+  {
+    immediate: false,
+  },
+)
+const { data: noteLists, refresh: refreshNote } = useAsyncData(
+  'dashboard-notes',
+  dashboardRepo.getNotes,
+  {
+    immediate: false,
+  },
 )
 
 const _profile = computed(() => {
@@ -38,11 +50,6 @@ const stat = computed(() => [
   },
 ])
 
-const { data: noteLists } = useAsyncData(
-  'dashboard-notes',
-  dashboardRepo.getNotes,
-)
-
 const notes = computed<PreviewNote[]>(() => {
   return (
     noteLists.value?.map((note) => ({
@@ -54,50 +61,60 @@ const notes = computed<PreviewNote[]>(() => {
     })) ?? []
   )
 })
+
+onMounted(() => {
+  refreshProjects()
+  refreshProfile()
+  refreshNote()
+})
 </script>
 
 <template>
-  <div class="min-h-screen">
-    <div
-      class="flex w-full gap-y-7 justify-between mb-4 md:mb-6 flex-col md:flex-row"
-    >
-      <div class="flex flex-col gap-y-1">
-        <h1
-          :title="profile?.name + 'workspace'"
-          class="text-[32px] leading-10 font-bold text-gray-10"
-        >
-          Welcome, <span class="block lg:inline">{{ profile?.name }}</span>
-        </h1>
-        <p class="text-sm leading-[18px] font-normal text-gray-10">
-          Keep track of your activities, notes, and
-          <span class="block lg:inline"> projects from your workspace.</span>
-        </p>
-      </div>
-      <div class="flex md:items-center flex-col md:flex-row gap-8">
-        <ul class="flex gap-x-12">
-          <li
-            v-for="s in stat"
-            :key="s.title"
-            class="flex flex-col items-center"
+  <ClientOnly>
+    <div class="min-h-screen">
+      <div
+        class="flex w-full gap-y-7 justify-between mb-4 md:mb-6 flex-col md:flex-row"
+      >
+        <div class="flex flex-col gap-y-1">
+          <h1
+            :title="profile?.name + 'workspace'"
+            class="text-[32px] leading-10 font-bold text-gray-10"
           >
-            <div class="text-sm leading-[18px] text-gray-10">{{ s.title }}</div>
-            <div class="text-[48px] leading-[56px] font-bold text-gray-10">
-              {{ s.value }}
-            </div>
-          </li>
-        </ul>
-        <UButton
-          class="rounded-[48px] min-w-[165px] w-[165px]"
-          leading-icon="humbleicons:plus"
-          label="Add project"
-        />
+            Welcome, <span class="block lg:inline">{{ profile?.name }}</span>
+          </h1>
+          <p class="text-sm leading-[18px] font-normal text-gray-10">
+            Keep track of your activities, notes, and
+            <span class="block lg:inline"> projects from your workspace.</span>
+          </p>
+        </div>
+        <div class="flex md:items-center flex-col md:flex-row gap-8">
+          <ul class="flex gap-x-12">
+            <li
+              v-for="s in stat"
+              :key="s.title"
+              class="flex flex-col items-center"
+            >
+              <div class="text-sm leading-[18px] text-gray-10">
+                {{ s.title }}
+              </div>
+              <div class="text-[48px] leading-[56px] font-bold text-gray-10">
+                {{ s.value }}
+              </div>
+            </li>
+          </ul>
+          <UButton
+            class="rounded-[48px] min-w-[165px] w-[165px]"
+            leading-icon="humbleicons:plus"
+            label="Add project"
+          />
+        </div>
+      </div>
+      <div class="grid gap-5 grid-cols-1 lg:grid-cols-[400px,1fr]">
+        <CardDashboardProfile :profile="(_profile as ProfileInformation)" />
+        <CardDashboardActivity />
+        <CardDashboardNote :notes="notes" />
+        <CardDashboardRecentProject :projects="projects ?? []" />
       </div>
     </div>
-    <div class="grid gap-5 grid-cols-1 lg:grid-cols-[400px,1fr]">
-      <CardDashboardProfile :profile="(_profile as ProfileInformation)" />
-      <CardDashboardActivity />
-      <CardDashboardNote :notes="notes" />
-      <CardDashboardRecentProject :projects="projects ?? []" />
-    </div>
-  </div>
+  </ClientOnly>
 </template>
